606c4800eb1a908fe02acbb80bdf1856d4e2cf9b
Yan Yi Goh <yy.git@protonmail.com>
2019-01-30 19:10:15 +0800

    Move Telegram bot code to own package
    main.go is now reduced to calling it from internal package

--- cmd/main.go
+++ cmd/main.go
@@ -1,45 +1,9 @@
 package main
 
 import (
-	"log"
-	"time"
-
-	"github.com/caarlos0/env"
-	"github.com/joho/godotenv"
-	telebot "gopkg.in/tucnak/telebot.v2"
+	"github.com/yanyi/go-telegram-bot/internal/app/telegrambot"
 )
 
-type config struct {
-	APIToken string `env:"API_TOKEN"`
-	APIURL   string `env:"API_URL" envDefault:"https://api.telegram.org"`
-	BotName  string `env:"BOT_NAME"`
-}
-
 func main() {
-	if err := godotenv.Load("configs/.env"); err != nil {
-		log.Fatalf("Can't load .env file. Error message: %v", err)
-	}
-
-	var cfg config
-	if err := env.Parse(&cfg); err != nil {
-		log.Fatalf("Can't parse environment variables. Error message: %v", err)
-	}
-
-	bot, err := telebot.NewBot(telebot.Settings{
-		Token:  cfg.APIToken,
-		URL:    cfg.APIURL,
-		Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
-	})
-
-	if err != nil {
-		log.Fatal("Can't load the Telegram Bot. Error message: ", err)
-	}
-
-	bot.Handle("/hello", func(m *telebot.Message) {
-		log.Printf("User: {%s} Message: {%s}", m.Sender.Username, m.Text)
-		bot.Send(m.Sender, "Hello world")
-	})
-
-	log.Printf("Bot {%s} started…", cfg.BotName)
-	bot.Start()
+	telegrambot.Start()
 }
